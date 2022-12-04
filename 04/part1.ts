b98bad12a8e5708bdbe03dbd5bf0405fda4fdd1c
Colin Bate <colin@colinbate.com>
2022-12-04 01:19:16 -0400

    Day 4

---
+++ 04/part1.ts
@@ -0,0 +1,13 @@
+import { readLines } from "https://deno.land/std@0.167.0/io/mod.ts";
+
+let total = 0;
+for await (const line of readLines(Deno.stdin)) {
+  const [first, second] = line.split(',');
+  const [x, y] = first.split('-').map(n => parseInt(n, 10));
+  const [a, b] = second.split('-').map(n => parseInt(n, 10));
+  if ((x <= a && y >= b) || (x >= a && y <= b)) {
+    total += 1;
+  }
+}
+
+console.log(total);
